3b8360c617c1d2a825ed3558a32673b7651378bc
Pi <rzheng@fau.edu>
2019-10-20 18:46:57 -0400

    added video stream to user interface, look in Development's control room

---
+++ Development/execute command line/child_process.js
@@ -0,0 +1,11 @@
+const { exec } = require('child_process');
+exec('mpg123 emergency_announcement.mp3', (err, stdout, stderr) => {
+  if (err) {
+    //some err occurred
+    console.error(err)
+  } else {
+   // the *entire* stdout and stderr (buffered)
+   console.log(`stdout: ${stdout}`);
+   console.log(`stderr: ${stderr}`);
+  }
+});
